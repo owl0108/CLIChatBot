95b0e12d00e57b2e7275fc3fb1541779f1dc2e28
Takuya Ishii <takuya5115@gmail.com>
2025-05-24 19:07:20 +0200

    updating the model to 3B quantized

--- backend/model.py
+++ backend/model.py
@@ -3,7 +3,7 @@ from llama_cpp import Llama
 def load_model() -> Llama: 
     print("Loading model...")
     llm = Llama.from_pretrained(
-        repo_id="unsloth/Llama-3.2-1B-Instruct-GGUF", filename="Llama-3.2-1B-Instruct-F16.gguf",
+        repo_id="unsloth/Llama-3.2-3B-Instruct-GGUF", filename="Llama-3.2-3B-Instruct-IQ4_NL.gguf",
         verbose=False
     )
     print("Model loaded.")
