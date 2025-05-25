3100cdb33814f9ace95d879d07aa9674f55c593e
Takuya Ishii <takuya5115@gmail.com>
2025-05-26 00:19:05 +0200

    make the server compatible with wifi off setting

--- src/backend/model.py
+++ src/backend/model.py
@@ -1,18 +1,27 @@
 from llama_cpp import Llama
+from .parameters import MAX_TOKENS, CHACHED_MODEL_PATH
 
 # TODO: separate model configuration from loading logic
-
-MAX_TOKENS = 10000  # Maximum for unsloth/Llama-3.2-3B-Instruct-GGUF/Llama-3.2-3B-Instruct-IQ4_NL.gguf is around 13k
-
-
 def load_model() -> Llama:
     print("Loading model...")
-    llm = Llama.from_pretrained(
-        repo_id="unsloth/Llama-3.2-3B-Instruct-GGUF",
-        filename="Llama-3.2-3B-Instruct-IQ4_NL.gguf",
-        verbose=False,
-        chat_format="llama-3",
-        n_ctx=MAX_TOKENS,  # goes up to 128k for llama-3.2
-    )
+    # check if the model is already cached
+    # from_pretrained will raise error without an internet connection
+    # even when not downloading the model
+    model_path = CHACHED_MODEL_PATH.expanduser().resolve()
+    if model_path.exists():
+        llm = Llama(
+            model_path=str(model_path),
+            verbose=False,
+            chat_format="llama-3",
+            n_ctx=MAX_TOKENS,
+        )
+    else:
+        llm = Llama.from_pretrained(
+            repo_id="unsloth/Llama-3.2-3B-Instruct-GGUF",
+            filename="Llama-3.2-3B-Instruct-IQ4_NL.gguf",
+            verbose=False,
+            chat_format="llama-3",
+            n_ctx=MAX_TOKENS,  # goes up to 128k for llama-3.2
+        )
     print("Model loaded.")
     return llm
--- src/backend/parameters.py
+++ src/backend/parameters.py
@@ -4,3 +4,8 @@ PROJECT_ROOT = Path(__file__).parent.parent.parent
 DATABASE_DIR = PROJECT_ROOT / "database"
 DATABASE_DIR.mkdir(exist_ok=True)  # Ensure the database directory exists
 DB_URL = f"sqlite:///{DATABASE_DIR / 'chat_history.db'}"  # Default database URL, can be overridden
+
+# Model parameters
+# Maximum for unsloth/Llama-3.2-3B-Instruct-GGUF/Llama-3.2-3B-Instruct-IQ4_NL.gguf is around 13k
+MAX_TOKENS = 10000
+CHACHED_MODEL_PATH = Path("~/.cache/huggingface/hub/models--unsloth--Llama-3.2-3B-Instruct-GGUF/snapshots/571c76bbd17f77e948aeda72fabfe31b9597864a/Llama-3.2-3B-Instruct-IQ4_NL.gguf")
